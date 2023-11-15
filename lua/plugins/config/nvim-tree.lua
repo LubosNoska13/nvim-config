dbb9ac675b9fe844982ab70c9b2ec5913ee9ef93
Lubos Noska <lubos.noska12@gmail.com>
2023-11-15 17:15:27 +0100

    Add keymaps to nvim-tree.lua file.

--- lua/plugins/config/nvim-tree.lua
+++ lua/plugins/config/nvim-tree.lua
@@ -12,5 +12,7 @@ tree.setup({
   }
 })
 
+local keymap = vim.keymap
 
-
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
+-- keymap.set("n", "<leader>ef", ":NvimTreeFocus<CR>") -- focus on file explorer

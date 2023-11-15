b57b15b59bc0d79a1dc6b779d52235edb846bc63
Lubos Noska <lubos.noska12@gmail.com>
2023-11-15 17:11:49 +0100

    Add keymaps to split windows in nvim.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,11 +1,18 @@
 local keymap = vim.keymap
 
+-- Make spacebar a leader key
 vim.g.mapleader = " "
 
 -- keymap.set("n", "<leader>eh", vim.cmd.Ex) 
 
 -- clear search highlights
 keymap.set('n', '<leader>nc', ':nohlsearch<CR>')
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
 -- move multiple line up or down
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
